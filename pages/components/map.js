9466c3029d6306570f8db7155f71b8388d387f74
Josh <joshuabowers.dev@gmail.com>
2022-07-15 03:41:10 +0200

    mapbox
    -minor improvements to map
    - made map component

---
+++ pages/components/map.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { useEffect } from "react";
+import tw from "tailwind-styled-components";
+import mapboxgl from "!mapbox-gl";
+
+mapboxgl.accessToken =
+  "pk.eyJ1Ijoiam9zaHVhYm93ZXJzIiwiYSI6ImNsNWxxM2pyYzBtZDMzZHFydXFjMDJjeDcifQ.rk1C8arE9nuIM6ywE4r5Cg";
+
+const Map = () => {
+  useEffect(() => {
+    const map = new mapboxgl.Map({
+      container: "map",
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: [-63.076094425521255, 18.094497883416697],
+      zoom: 11.5,
+    });
+  });
+
+  return <Wrapper id="map"></Wrapper>;
+};
+
+export default Map;
+
+const Wrapper = tw.div`
+    flex-1
+`;
--- pages/index.js
+++ pages/index.js
@@ -2,35 +2,12 @@ import { useEffect } from "react";
 import Head from "next/head";
 import Image from "next/image";
 import tw from "tailwind-styled-components";
-import mapboxgl from "!mapbox-gl";
+import Map from "./components/Map";
 
-mapboxgl.accessToken =
-  "pk.eyJ1Ijoiam9zaHVhYm93ZXJzIiwiYSI6ImNsNWxxM2pyYzBtZDMzZHFydXFjMDJjeDcifQ.rk1C8arE9nuIM6ywE4r5Cg";
-
-export default function Home() {
-  useEffect(() => {
-    console.log("gm");
-  }, []);
-
-  useEffect(() => {
-    const map = new mapboxgl.Map({
-      container: "map",
-      style: "mapbox://styles/mapbox/streets-v11",
-      center: [-63.076094425521255, 18.094497883416697], 
-      zoom: 11.5,
-    });
-  });
-
-  // const map = new.mapboxgl.Map({
-  //   container: 'map',
-  //   style: '',
-  //   center: ,
-  //   zoom: ,
-  // })
-
+export default function Home() { 
   return (
     <Wrapper>
-      <Map id="map">Map</Map>
+      <Map />
       <ActionItems>Start</ActionItems>
     </Wrapper>
   );
@@ -38,10 +15,6 @@ export default function Home() {
 
 const Wrapper = tw.div`
   flex flex-col bg-red-300 h-screen
-`;
-
-const Map = tw.div`
-  bg-red-500 flex-1
 `;
 
 const ActionItems = tw.div`
