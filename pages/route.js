0f4d7779855afd0b337bb9dbd9d6a2b1a36e6b6c
Josh <joshuabowers.dev@gmail.com>
2022-07-21 03:33:07 +0200

    route page back alive
    - mainly focused on clearing css and making sure  "/route" can display
    - next task will be to add dropdown for destination /location fields

--- pages/route.js
+++ pages/route.js
@@ -1,4 +1,4 @@
-import { useEffect} from "react";
+import { useEffect } from "react";
 import Head from "next/head";
 import Image from "next/image";
 import tw from "tailwind-styled-components";
@@ -15,13 +15,13 @@ export default function Route() {
         </Link>
         {/* <ButtonContainer>
           <Link href="/"> */}
-            {/* <BackButton src="https://i.pinimg.com/564x/c4/c8/c4/c4c8c425bee8fb9e9af842a865442a57.jpg" /> */}
-            {/* <BackButton src="https://cdn-icons.flaticon.com/png/512/2040/premium/2040514.png?token=exp=1658074793~hmac=a9eea04ab4691db8cc17b3e54c292a63" />
+        {/* <BackButton src="https://i.pinimg.com/564x/c4/c8/c4/c4c8c425bee8fb9e9af842a865442a57.jpg" /> */}
+        {/* <BackButton src="https://cdn-icons.flaticon.com/png/512/2040/premium/2040514.png?token=exp=1658074793~hmac=a9eea04ab4691db8cc17b3e54c292a63" />
           </Link>
         </ButtonContainer> */}
         <Profile>
-          <Name>Rolando Brison</Name>
-          <UserImage src="https://scontent-ams4-1.xx.fbcdn.net/v/t39.30808-6/240593076_10165775712425088_6366602392358673388_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=m0XMRi9IFIQAX9PkZio&_nc_ht=scontent-ams4-1.xx&oh=00_AT_RqYnWqr0Q1khMmH-UvOVXYa2fUaKQrTjclT_rJCbmsg&oe=62D63234" />
+          <Name>Melissa Richardson</Name>
+          <UserImage src="https://images.unsplash.com/photo-1589156191108-c762ff4b96ab?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1372&q=80" />
         </Profile>
       </Header>
       {/* Map */}
@@ -51,7 +51,7 @@ export default function Route() {
           <RouteActions>
             <FromToIcons>
               <OriginIcon src="https://cdn-icons-png.flaticon.com/512/6686/6686693.png" />
-              <Line src="https://cdn-icons.flaticon.com/png/512/2990/premium/2990154.png?token=exp=1658092304~hmac=9902be6761ab3c3bd3c09f9a49d7de3a" />
+              <Line src="https://cdn-icons.flaticon.com/png/512/2652/premium/2652705.png?token=exp=1658366464~hmac=289a3e744afbd901c4d7cd74adbb1fd4" />
               <DestinationIcon src="https://cdn-icons-png.flaticon.com/512/447/447031.png" />
             </FromToIcons>
             <LocationBoxes>
@@ -60,9 +60,7 @@ export default function Route() {
             </LocationBoxes>
           </RouteActions>
           <SearchSection>
-            <SearchButton>
-              Search
-            </SearchButton>
+            <SearchButton>Search</SearchButton>
           </SearchSection>
         </RouteContainer>
       </ActionItems>
@@ -75,16 +73,16 @@ export default function Route() {
 
 // Tailwind Style components
 const Wrapper = tw.div`
-flex flex-col h-screen pl-10 pr-10 pt-2
+flex flex-col h-screen
 `;
 
 // Navbar
 const Header = tw.div`
-flex justify-between items-center pr-4 border-b-2 border-black pb-2
+flex justify-between items-center border-b-4 border-black px-4 py-4 
 `;
 
 const Logo = tw.img`
-h-10
+h-14
 `;
 
 const Profile = tw.div`
@@ -96,15 +94,15 @@ mr-4 w-35
 `;
 
 const UserImage = tw.img`
-h-12 w-12 rounded-full
+h-14 w-14 rounded-full object-contain border border-black
 `;
 // User section
 const ActionItems = tw.div`
-flex-1 flex flex-row border border-black 
+flex-1 flex flex-row border border-t-black border-t-4 border-b-black border-b-4
 `;
 
 const ActionButtons = tw.div`
-flex flex-col items-center w-1/6 justify-center justify-between my-36 mx-16
+flex flex-col w-1/6 justify-between border h-full items-center py-10
 `;
 
 const ActionButton = tw.div`
@@ -132,11 +130,11 @@ w-5/6 flex flex-col border border-l-black
 `;
 // Around the icons and inputs
 const RouteActions = tw.div`
-flex h-3/4 justify-center 
+flex h-3/4 justify-center h-full
 `;
 // Around all the From to Icons
 const FromToIcons = tw.div`
-md:w-48 lg:w-48 p-6 justify-center my-24
+md:w-32 lg:w-32 border
 `;
 
 const OriginIcon = tw.img`
@@ -144,24 +142,24 @@ object-contain scale-75
 `;
 
 const Line = tw.img`
-scale-50 rotate-90 p-4 object-contain
+scale-50 rotate-90 object-contain
 `;
 
 const DestinationIcon = tw.img`
-p-4 object-contain scale-75
+object-contain scale-75
 `;
 // Around location input fields
 const LocationBoxes = tw.div`
-flex flex-col justify-between py-32
+flex flex-col border
 `;
 
 //Search section
 const SearchSection = tw.div`
-flex-1
+flex border justify-center h-1/4
 `;
 
 const SearchButton = tw.div`
-bg-black text-white text-center mt-6 py-8 mx-4 text-2xl rounded-md w-4/5 ml-20
+bg-black text-white text-center text-2xl rounded-md w-3/5 p-6 m-6
 `;
 
 // Text box
