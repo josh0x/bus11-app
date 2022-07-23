7015dfa80ab640e609b5a68cb8e090990d93437d
Josh <joshuabowers.dev@gmail.com>
2022-07-23 02:39:59 +0200

    Mobile layout v1.0
    - menu buttons at top(3)
    - main router actions 3/4 length
    - search button at button

--- pages/route.js
+++ pages/route.js
@@ -35,10 +35,7 @@ export default function Route() {
         <Link href="/">
           <Logo src="https://i.pinimg.com/originals/c2/98/27/c2982793e4e308c03c9800c4a99f363d.jpg" />
         </Link>
-        <Profile>
-          <Name>Kim Woods</Name>
-          <UserImage src="https://images.unsplash.com/photo-1589156191108-c762ff4b96ab?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1372&q=80" />
-        </Profile>
+        <BurgerMenu src="https://cdn-icons.flaticon.com/png/512/4254/premium/4254068.png?token=exp=1658535041~hmac=92e4d47bd94b0e64dbec75d9be49d812" />
       </Header>
       {/* Map */}
 
@@ -48,90 +45,147 @@ export default function Route() {
           <Link href="/route">
             <ActionButton>
               <ActionButtonImage src="https://cdn-icons-png.flaticon.com/512/4317/4317907.png" />
-              Bus Route
+              {/* Bus Route */}
             </ActionButton>
           </Link>
           <ActionButton>
             <ActionButtonImage src="https://cdn-icons-png.flaticon.com/512/8059/8059503.png" />
-            Radio
+            {/* Radio */}
           </ActionButton>
           <ActionButton>
             <ActionButtonImage src="https://cryptologos.cc/logos/usd-coin-usdc-logo.png?v=022" />
-            Donation
+            {/* Donation */}
           </ActionButton>
         </ActionButtons>
+
         {/* Route Container */}
         <RouteContainer>
-          <RouteActions>
-            <FromToIcons>
-              {/* Origin and dropdown menu */}
-              <OriginIcon src="https://cdn-icons-png.flaticon.com/512/6686/6686693.png" />
-
-              <div className=" mt-10 w-48 bg-black rounded-md">
-                <Listbox value={selected} onChange={setSelected}>
-                  <div className="relative">
-                    <Listbox.Button className="relative w-full text-xl text-white cursor-default rounded-md bg-black py-4 mx-10 text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-black md:text-md">
-                      <span className=" block truncate">{selected.name}</span>
-                      <span className=" pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2 ">
-                        <SelectorIcon
-                          className="h-7 w-7 text-white"
-                          aria-hidden="true"
-                        />
-                      </span>
-                    </Listbox.Button>
-                    <Transition
-                      as={Fragment}
-                      leave="transition ease-in duration-100"
-                      leaveFrom="opacity-100"
-                      leaveTo="opacity-0"
-                    >
-                      <Listbox.Options className="absolute mt-1 max-h-60 w-full ml-4 overflow-auto rounded-md bg-black py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none md:text-md">
-                        {locations.map((location, locationIdx) => (
-                          <Listbox.Option
-                            key={locationIdx}
-                            className={({ active }) =>
-                              `relative cursor-default select-none py-2 pl-10 pr-4 ${
-                                active
-                                  ? "bg-blue-500 text-white"
-                                  : "bg-black text-white"
-                              }`
-                            }
-                            value={location}
-                          >
-                            {({ selected }) => (
-                              <>
-                                <span
-                                  className={`block truncate ${
-                                    selected ? "font-medium" : "font-normal"
-                                  }`}
-                                >
-                                  {location.name}
+          <FromToIcons>
+            {/* Origin and dropdown menu */}
+            <LocationIcon src="https://cdn-icons-png.flaticon.com/512/6686/6686693.png" />
+            <div className=" mt-10 w-64 bg-black rounded-md">
+              <Listbox value={selected} onChange={setSelected}>
+                <div className="relative">
+                  <Listbox.Button className="relative w-full text-3xl text-white cursor-default rounded-md bg-black py-8 mx-10 text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-black md:text-md">
+                    <span className=" block truncate">{selected.name}</span>
+                    <span className=" pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2 ">
+                      <SelectorIcon
+                        className="h-12 w-12 mx-4 text-white"
+                        aria-hidden="true"
+                      />
+                    </span>
+                  </Listbox.Button>
+                  <Transition
+                    as={Fragment}
+                    leave="transition ease-in duration-100"
+                    leaveFrom="opacity-100"
+                    leaveTo="opacity-0"
+                  >
+                    <Listbox.Options className="absolute mt-1 max-h-60 w-full ml-4 overflow-auto rounded-md bg-black py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none md:text-md">
+                      {locations.map((location, locationIdx) => (
+                        <Listbox.Option
+                          key={locationIdx}
+                          className={({ active }) =>
+                            `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                              active
+                                ? "bg-blue-500 text-white"
+                                : "bg-black text-white"
+                            }`
+                          }
+                          value={location}
+                        >
+                          {({ selected }) => (
+                            <>
+                              <span
+                                className={`block truncate ${
+                                  selected ? "font-medium" : "font-normal"
+                                }`}
+                              >
+                                {location.name}
+                              </span>
+                              {selected ? (
+                                <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-white">
+                                  <CheckIcon
+                                    className="h-5 w-5"
+                                    aria-hidden="true"
+                                  />
                                 </span>
-                                {selected ? (
-                                  <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-white">
-                                    <CheckIcon
-                                      className="h-5 w-5"
-                                      aria-hidden="true"
-                                    />
-                                  </span>
-                                ) : null}
-                              </>
-                            )}
-                          </Listbox.Option>
-                        ))}
-                      </Listbox.Options>
-                    </Transition>
-                  </div>
-                </Listbox>
-              </div>
-
-              {/* <Line src="https://cdn-icons.flaticon.com/png/512/2652/premium/2652705.png?token=exp=1658366464~hmac=289a3e744afbd901c4d7cd74adbb1fd4" /> */}
-            </FromToIcons>
-          </RouteActions>
-          <SearchSection>
-            <SearchButton>Search</SearchButton>
-          </SearchSection>
+                              ) : null}
+                            </>
+                          )}
+                        </Listbox.Option>
+                      ))}
+                    </Listbox.Options>
+                  </Transition>
+                </div>
+              </Listbox>
+            </div>
+          </FromToIcons>
+          <FromToIcons>
+            <LocationIcon src="https://cdn-icons-png.flaticon.com/512/447/447031.png" />
+            <div className=" mt-10 w-64 bg-black rounded-md">
+              <Listbox value={selected} onChange={setSelected}>
+                <div className="relative">
+                  <Listbox.Button className="relative w-full text-3xl text-white cursor-default rounded-md bg-black py-8 mx-10 text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-black md:text-md">
+                    <span className=" block truncate">{selected.name}</span>
+                    <span className=" pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2 ">
+                      <SelectorIcon
+                        className="h-12 w-12 mx-4 text-white"
+                        aria-hidden="true"
+                      />
+                    </span>
+                  </Listbox.Button>
+                  <Transition
+                    as={Fragment}
+                    leave="transition ease-in duration-100"
+                    leaveFrom="opacity-100"
+                    leaveTo="opacity-0"
+                  >
+                    <Listbox.Options className="absolute mt-1 max-h-60 w-full ml-4 overflow-auto rounded-md bg-black py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none md:text-md">
+                      {locations.map((location, locationIdx) => (
+                        <Listbox.Option
+                          key={locationIdx}
+                          className={({ active }) =>
+                            `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                              active
+                                ? "bg-blue-500 text-white"
+                                : "bg-black text-white"
+                            }`
+                          }
+                          value={location}
+                        >
+                          {({ selected }) => (
+                            <>
+                              <span
+                                className={`block truncate ${
+                                  selected ? "font-medium" : "font-normal"
+                                }`}
+                              >
+                                {location.name}
+                              </span>
+                              {selected ? (
+                                <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-white">
+                                  <CheckIcon
+                                    className="h-5 w-5"
+                                    aria-hidden="true"
+                                  />
+                                </span>
+                              ) : null}
+                            </>
+                          )}
+                        </Listbox.Option>
+                      ))}
+                    </Listbox.Options>
+                  </Transition>
+                </div>
+              </Listbox>
+            </div>
+            {/* <Line src="https://cdn-icons.flaticon.com/png/512/2652/premium/2652705.png?token=exp=1658366464~hmac=289a3e744afbd901c4d7cd74adbb1fd4" /> */}
+          </FromToIcons>
         </RouteContainer>
+        <SearchSection>
+          <SearchButton>Search</SearchButton>
+        </SearchSection>
       </ActionItems>
     </Wrapper>
   );
@@ -151,81 +205,55 @@ const Logo = tw.img`
 h-14
 `;
 
-const Profile = tw.div`
-flex items-center
-`;
-
 const Name = tw.div`
-mr-4 w-35
+mr-6 font-bold text-xl
 `;
 
-const UserImage = tw.img`
-h-14 w-14 rounded-full object-contain border border-black
+const BurgerMenu = tw.img`
+h-14 w-14 object-contain 
 `;
 // User section
+// "Action items" has the buttons: radio, bus route, donation
 const ActionItems = tw.div`
- border border-t-black border-t-4 border-b-black border-b-4
+h-screen
 `;
 
 const ActionButtons = tw.div`
-flex justify-between border items-center
+flex justify-center gap-14 my-4 
 `;
 
 const ActionButton = tw.div`
-flex border  items-center flex-col justify-center transform hover:scale-110 transition text-xl w-64 shadow-md rounded-sm
+flex items-center border-black border-4 justify-center flex-col transform hover:scale-110 transition shadow-lg rounded-md font-sans font-bold w-24 h-24 p-2
 `;
 
 const ActionButtonImage = tw.img`
-
-`;
-// Text bottom
-const Footer = tw.div`
-h-20 text-2xl p-4 border border-black font-helvetica text-center
-`;
-// Back button
-const ButtonContainer = tw.div`
-
 `;
 
-const BackButton = tw.img`
-h-12 scale-75 object-contain rotate-180 
-`;
 // The entire route div
+// Actually the icons and dropdown menus
 const RouteContainer = tw.div`
- flex flex-col border border-l-black h-full h-screen
-`;
-// Around the icons and inputs
-const RouteActions = tw.div`
-flex justify-center h-full border
+ flex border border-black gap-10 mx-10 pb-64 my-10
 `;
 // Around all the From to Icons
 const FromToIcons = tw.div`
-border
+border border-black w-full 
 `;
 
-const OriginIcon = tw.img`
-object-contain scale-75 border
+const LocationIcon = tw.img`
+object-contain h-40
 `;
 
 const Line = tw.img`
 scale-50 rotate-90 object-contain
 `;
 
-const DestinationIcon = tw.img`
-md:w-32 lg:w-32 object-contain scale-52
-`;
-// Around location input fields
-const LocationBoxes = tw.div`
-flex flex-col border
-`;
-
 //Search section
 const SearchSection = tw.div`
-border
+p-10
 `;
 
 const SearchButton = tw.div`
-bg-black text-white text-center text-3xl rounded-md w-3/5 p-6
+bg-black text-white text-center text-3xl rounded-md  p-6
 `;
 
 // Text box
