24681ab4eb416c9a8e9c156a282ec24f8fbd9e22
Josh <joshuabowers.dev@gmail.com>
2022-07-23 05:54:08 +0200

    improved mobile layout!

--- pages/route.js
+++ pages/route.js
@@ -2,7 +2,6 @@ import { useEffect } from "react";
 import Head from "next/head";
 import Image from "next/image";
 import tw from "tailwind-styled-components";
-
 import Link from "next/link";
 import { Fragment, useState } from "react";
 import { Listbox, Transition } from "@headlessui/react";
@@ -34,10 +33,9 @@ export default function Route() {
         <BurgerMenu src="https://cdn-icons.flaticon.com/png/512/4254/premium/4254068.png?token=exp=1658535041~hmac=92e4d47bd94b0e64dbec75d9be49d812" />
       </Header>
       {/* Map */}
- 
+
       {/* User section */}
       <ActionItems>
-
         {/* Route Container */}
         <RouteContainer>
           <FromToIcons>
@@ -46,9 +44,9 @@ export default function Route() {
             <div className=" w-64 bg-black rounded-md">
               <Listbox value={selected} onChange={setSelected}>
                 <div className="relative">
-                  <Listbox.Button className="relative w-full text-3xl text-white cursor-default rounded-md bg-black py-8 mx-10 text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-black md:text-md">
+                  <Listbox.Button className="relative w-full text-3xl text-white cursor-default rounded-md bg-black py-8 mx-14 text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-black">
                     <span className=" block truncate">{selected.name}</span>
-                    <span className=" pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2 ">
+                    <span className=" pointer-events-none absolute inset-y-0 right-0 flex items-center ">
                       <SelectorIcon
                         className="h-12 w-12 mx-4 text-white"
                         aria-hidden="true"
@@ -61,12 +59,12 @@ export default function Route() {
                     leaveFrom="opacity-100"
                     leaveTo="opacity-0"
                   >
-                    <Listbox.Options className="absolute mt-1 max-h-60 w-full ml-4 overflow-auto rounded-md bg-black py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none md:text-md">
+                    <Listbox.Options className="absolute m-2 w-full ml-4 overflow-auto rounded-md bg-black py-2 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none text-3xl">
                       {locations.map((location, locationIdx) => (
                         <Listbox.Option
                           key={locationIdx}
                           className={({ active }) =>
-                            `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                            `relative cursor-default select-none py-4 pl-10 ${
                               active
                                 ? "bg-blue-500 text-white"
                                 : "bg-black text-white"
@@ -102,14 +100,20 @@ export default function Route() {
             </div>
           </FromToIcons>
 
+          {/* TRANSFER ICONS / BUTTON */}
+          <TransferDiv>
+            <TransferIcon src="https://cdn-icons.flaticon.com/png/512/2652/premium/2652705.png?token=exp=1658546836~hmac=7ccb0c66b8c8d0940ff90f65a8075635" />
+          </TransferDiv>
+
+          {/* Destination and dropdown menu */}
           <FromToIcons>
             <DestinationIcon src="https://cdn-icons-png.flaticon.com/512/447/447031.png" />
-            <div className="w-64 bg-black rounded-md">
+            <div className=" w-64 bg-black rounded-md">
               <Listbox value={selected} onChange={setSelected}>
                 <div className="relative">
-                  <Listbox.Button className="relative w-full text-3xl text-white cursor-default rounded-md bg-black py-8 mx-10 text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-black md:text-md">
+                  <Listbox.Button className="relative w-full text-3xl text-white cursor-default rounded-md bg-black py-8 mx-14 text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-black">
                     <span className=" block truncate">{selected.name}</span>
-                    <span className=" pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2 ">
+                    <span className=" pointer-events-none absolute inset-y-0 right-0 flex items-center ">
                       <SelectorIcon
                         className="h-12 w-12 mx-4 text-white"
                         aria-hidden="true"
@@ -122,12 +126,12 @@ export default function Route() {
                     leaveFrom="opacity-100"
                     leaveTo="opacity-0"
                   >
-                    <Listbox.Options className="absolute mt-1 max-h-60 w-full ml-4 overflow-auto rounded-md bg-black py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none md:text-md">
+                    <Listbox.Options className="absolute m-2 w-full ml-4 overflow-auto rounded-md bg-black py-2 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none text-3xl">
                       {locations.map((location, locationIdx) => (
                         <Listbox.Option
                           key={locationIdx}
                           className={({ active }) =>
-                            `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                            `relative cursor-default select-none py-4 pl-10 ${
                               active
                                 ? "bg-blue-500 text-white"
                                 : "bg-black text-white"
@@ -161,7 +165,6 @@ export default function Route() {
                 </div>
               </Listbox>
             </div>
-            {/* <Line src="https://cdn-icons.flaticon.com/png/512/2652/premium/2652705.png?token=exp=1658366464~hmac=289a3e744afbd901c4d7cd74adbb1fd4" /> */}
           </FromToIcons>
         </RouteContainer>
         <SearchSection>
@@ -170,22 +173,21 @@ export default function Route() {
       </ActionItems>
 
       <ActionButtons>
-          <Link href="/route">
-            <ActionButton>
-              <ActionButtonImage src="https://cdn-icons-png.flaticon.com/512/4317/4317907.png" />
-              {/* Bus Route */}
-            </ActionButton>
-          </Link>
+        <Link href="/route">
           <ActionButton>
-            <ActionButtonImage src="https://cdn-icons-png.flaticon.com/512/8059/8059503.png" />
-            {/* Radio */}
+            <ActionButtonImage src="https://cdn-icons-png.flaticon.com/512/4317/4317907.png" />
+            {/* Bus Route */}
           </ActionButton>
-          <ActionButton>
-            <ActionButtonImage src="https://cryptologos.cc/logos/usd-coin-usdc-logo.png?v=022" />
-            {/* Donation */}
-          </ActionButton>
-        </ActionButtons>
-
+        </Link>
+        <ActionButton>
+          <ActionButtonImage src="https://cdn-icons-png.flaticon.com/512/8059/8059503.png" />
+          {/* Radio */}
+        </ActionButton>
+        <ActionButton>
+          <ActionButtonImage src="https://cryptologos.cc/logos/usd-coin-usdc-logo.png?v=022" />
+          {/* Donation */}
+        </ActionButton>
+      </ActionButtons>
     </Wrapper>
   );
 }
@@ -197,7 +199,7 @@ flex flex-col h-screen
 
 // Navbar
 const Header = tw.div`
-flex justify-between items-center border-b-4 border-black px-4 py-4 
+flex justify-between items-center border-b-4 border-black px-4 py-4
 `;
 
 const Logo = tw.img`
@@ -210,11 +212,11 @@ h-14 w-14 object-contain
 // User section
 // "Action items" has the buttons: radio, bus route, donation
 const ActionItems = tw.div`
-
+h-4/5 border 
 `;
 
 const ActionButtons = tw.div`
-flex justify-center gap-14 py-6 border-t-4 border-black
+flex justify-center gap-14 py-6 border-t-4 border-black 
 `;
 
 const ActionButton = tw.div`
@@ -227,29 +229,37 @@ const ActionButtonImage = tw.img`
 // The entire route div
 // Actually the icons and dropdown menus
 const RouteContainer = tw.div`
-flex objects-contain justify-center gap-24
+flex objects-contain justify-center justify-between border h-5/6 p-28
 `;
 // Around all the From to Icons
 const FromToIcons = tw.div`
-my-20 pb-72  
+border 
+`;
+
+const TransferDiv = tw.div`
+border justify-center flex 
 `;
 
 const LocationIcon = tw.img`
-object-contain h-36 w-36 ml-20
+object-contain h-40 
 `;
 
 const LocationDropdown = tw.div`
 `;
 
 const DestinationIcon = tw.img`
-object-contain h-36 w-32 ml-20
+object-contain h-40 p-6
+`;
+
+const TransferIcon = tw.img`
+object-contain object-scale-down h-16 mt-16  
 `;
 
 //Search section
 const SearchSection = tw.div`
-p-10 pt-60
+
 `;
 
 const SearchButton = tw.div`
-bg-black text-white text-center text-3xl rounded-md p-6
+bg-black text-white text-center text-4xl rounded-md m-10 p-8 
 `;
